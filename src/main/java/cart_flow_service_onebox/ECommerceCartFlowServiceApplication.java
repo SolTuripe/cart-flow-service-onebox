e5392d8b9e5369a0e505338fb9bf5d6b470d5f23
solturipe <sol.turipe@apartool.com>
2024-12-05 23:28:22 +0100

    firts commit

---
+++ src/main/java/cart_flow_service_onebox/ECommerceCartFlowServiceApplication.java
@@ -0,0 +1,13 @@
+package cart_flow_service_onebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCartFlowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceCartFlowServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cart_flow_service_onebox/ECommerceCartFlowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cart_flow_service_onebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceCartFlowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

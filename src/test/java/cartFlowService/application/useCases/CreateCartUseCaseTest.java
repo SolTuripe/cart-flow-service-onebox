995b77143a7270a5f868384a42ca0c6de119acde
solturipe <sol.turipe@apartool.com>
2024-12-08 13:28:54 +0100

    add test shouldCreateCart

---
+++ src/test/java/cartFlowService/application/useCases/CreateCartUseCaseTest.java
@@ -0,0 +1,34 @@
+package cartFlowService.application.useCases;
+
+import cartFlowService.domain.models.Cart;
+import cartFlowService.domain.models.Item;
+import cartFlowService.domain.storage.CartRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class CreateCartUseCaseTest {
+
+    private final CartRepository cartRepository;
+    private final CreateCart createCart;
+
+    CreateCartUseCaseTest() {
+        this.cartRepository = mock(CartRepository.class);
+        this.createCart     = new CreateCart(cartRepository);
+    }
+
+    @Test
+    void shouldCreateCart() {
+        Item item                = new Item(22, "party shoes", 25.99);
+        ArrayList<Item> itemList = new ArrayList<>();
+        itemList.add(item);
+
+        createCart.createCart(itemList);
+
+        verify(cartRepository).createCart(any(Cart.class));
+    }
+}

f3cecc2983b504708777dad622589399b979d877
solturipe <sol.turipe@apartool.com>
2024-12-08 23:45:48 +0100

    new test shouldFailIfCartDoesNotExist and getCartUseCaseTest

--- src/test/java/cartFlowService/application/useCases/DeleteCartUseCaseTest.java
+++ src/test/java/cartFlowService/application/useCases/DeleteCartUseCaseTest.java
@@ -4,19 +4,20 @@ import cartFlowService.domain.errors.CartNotFoundError;
 import cartFlowService.domain.models.CartMaskId;
 import cartFlowService.domain.storage.CartRepository;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 public class DeleteCartUseCaseTest {
 
     private final CartRepository cartRepository;
-    private final DeleteCart deleteCart;
+    private final DeleteCart     deleteCart;
 
     DeleteCartUseCaseTest() {
         this.cartRepository = mock(CartRepository.class);
         this.deleteCart     = new DeleteCart(cartRepository);
     }
-
 
     @Test
     void shouldDeleteCartFromRepositoryById() throws CartNotFoundError {
@@ -26,6 +27,14 @@ public class DeleteCartUseCaseTest {
         deleteCart.deleteCart(cartMaskId.value);
 
         verify(cartRepository).deleteCartByMaskId(any(CartMaskId.class));
+    }
+
+    @Test
+    void shouldFailIfCartDoesNotExist() {
+        CartMaskId cartMaskId = new CartMaskId("6e55c340-9992-4d09-8986-8c19fc712f0b");
+        when(cartRepository.findMakId(cartMaskId)).thenReturn(false);
+
+        assertThrows(CartNotFoundError.class, () -> deleteCart.deleteCart(cartMaskId.value));
     }
 
 }
--- src/test/java/cartFlowService/application/useCases/GetCartUseCaseTest.java
+++ src/test/java/cartFlowService/application/useCases/GetCartUseCaseTest.java
@@ -1,10 +1,53 @@
 package cartFlowService.application.useCases;
 
+import cartFlowService.domain.errors.CartNotFoundError;
+import cartFlowService.domain.models.Cart;
+import cartFlowService.domain.models.CartMaskId;
+import cartFlowService.domain.models.Item;
 import cartFlowService.domain.storage.CartRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GetCartUseCaseTest {
 
-   // private final CartRepository cartRepository;
+    private final CartRepository cartRepository;
+    private final GetCart        getCart;
 
+    GetCartUseCaseTest() {
+        this.cartRepository = mock(CartRepository.class);
+        this.getCart        = new GetCart(cartRepository);
+    }
+
+    @Test
+    void shouldGetCartById() throws CartNotFoundError {
+        CartMaskId cartId = new CartMaskId("6e55c340-9992-4d09-8986-8c19fc712f0b");
+        Item item      = new Item(12, "party shoes", 25.99);
+
+        ArrayList<Item> itemList = new ArrayList<>();
+        itemList.add(item);
+
+        Cart newCart = new Cart(cartId, itemList);
+
+        when(cartRepository.findMakId(any(CartMaskId.class))).thenReturn(true);
+        getCart.getCartById(cartId.value);
+
+        assertEquals(cartId, newCart.getId());
+    }
+
+    @Test
+    void shouldFailIfCartDoesNotExist() {
+        CartMaskId cartMaskId = new CartMaskId("6e55c340-9992-4d09-8986-8c19fc712f0b");
+
+        when(cartRepository.findMakId(any(CartMaskId.class))).thenReturn(false);
+
+        assertThrows(CartNotFoundError.class, () -> getCart.getCartById(cartMaskId.value));
+    }
 
 }
